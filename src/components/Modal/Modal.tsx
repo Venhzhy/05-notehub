b81b2e034eda4e525c0fc2fcdfbff8d9dec328a9
Stepan_Venhzhyn <ids3688@gmail.com>
2025-11-19 19:17:53 +0200

    fix

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -19,8 +19,12 @@ const Modal = ({ children, onClose }: ModalProps) => {
 
     window.addEventListener("keydown", handleKeyDown);
 
+    const previousOverflow = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = previousOverflow;
     };
   }, [onClose]);
 
